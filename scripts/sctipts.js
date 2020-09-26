7a1dcc8f0abf57b1c4c4125e155170396d15cc32
Artem Koldunov <smorkwriter@yandex.ru>
2020-09-26 19:00:05 +0300

    fix: Сделать общую функцию открытия попапов

--- scripts/sctipts.js
+++ scripts/sctipts.js
@@ -1,8 +1,9 @@
 const popup = document.querySelector('.popup')
+const popupEdit = document.querySelector('.popup_edit')
 const popupOpenEditButton = document.querySelector('.profile__edit-button')
-const popupCloseEditButton = document.querySelector('.popup__button-close')
-const popupSaveEditButton = document.querySelector('.popup__button-save')
-const formElementEdit = document.querySelector('.popup__form_edit');
+const popupCloseEditButton = popupEdit.querySelector('.popup__button-close')
+const popupSaveEditButton = popupEdit.querySelector('.popup__button-save')
+const formElementEdit = popupEdit.querySelector('.popup__form_edit');
 const nameProfile = document.querySelector('.profile__name');
 const statusProfile = document.querySelector('.profile__status');
 const nameInput = document.getElementsByName('name-input')[0];
@@ -11,20 +12,25 @@ const statusInput = document.getElementsByName('status-input')[0];
 function openPopupEdit() {
     nameInput.value = nameProfile.textContent;
     statusInput.value = statusProfile.textContent;
-    popup.classList.add('popup_is-opened');
+    openPopup(popupEdit)
 }
 
-function savePopupEdit(evt) {
-    evt.preventDefault();
+function savePopupEdit() {
+    event.preventDefault();
     nameProfile.textContent = nameInput.value;
     statusProfile.textContent = statusInput.value;
-    closePopup(evt);
+    closePopup(popupEdit);
 }
 
-function closePopup(evt) {
-    popup.classList.remove('popup_is-opened');
-}
 
 popupOpenEditButton.addEventListener('click', openPopupEdit);
 formElementEdit.addEventListener('submit', savePopupEdit);
 popupCloseEditButton.addEventListener('click', closePopup);
+
+function openPopup() {
+    popup.classList.add('popup_is-opened');
+}
+
+function closePopup() {
+    popup.classList.remove('popup_is-opened');
+}
