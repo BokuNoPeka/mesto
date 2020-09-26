783b0dd8f69501b72f2f0ce399047a5586871d68
Artem Koldunov <smorkwriter@yandex.ru>
2020-09-26 17:09:04 +0300

    feat: Сделать адаптим для попапов

--- scripts/sctipts.js
+++ scripts/sctipts.js
@@ -2,7 +2,7 @@ const popup = document.querySelector('.popup')
 const popupOpenEditButton = document.querySelector('.profile__edit-button')
 const popupCloseEditButton = document.querySelector('.popup__button-close')
 const popupSaveEditButton = document.querySelector('.popup__button-save')
-const formElementEdit = document.querySelector('.popup__form');
+const formElementEdit = document.querySelector('.popup__form_edit');
 const nameProfile = document.querySelector('.profile__name');
 const statusProfile = document.querySelector('.profile__status');
 const nameInput = document.getElementsByName('name-input')[0];
@@ -18,13 +18,13 @@ function savePopupEdit(evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     statusProfile.textContent = statusInput.value;
-    closePopupEdit(evt);
+    closePopup(evt);
 }
 
-function closePopupEdit(evt) {
+function closePopup(evt) {
     popup.classList.remove('popup_is-opened');
 }
 
 popupOpenEditButton.addEventListener('click', openPopupEdit);
 formElementEdit.addEventListener('submit', savePopupEdit);
-popupCloseEditButton.addEventListener('click', closePopupEdit);
+popupCloseEditButton.addEventListener('click', closePopup);
