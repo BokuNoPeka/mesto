e8198ec13af15588362900b54e7a72eb0406b748
Artem Koldunov <smorkwriter@yandex.ru>
2020-10-03 19:56:23 +0300

    commit

--- scripts/sctipts.js
+++ scripts/sctipts.js
@@ -2,38 +2,12 @@ const popup = document.querySelector('.popup');
 const popupEdit = document.querySelector('.popup_edit');
 const popupOpenEditButton = document.querySelector('.profile__edit-button');
 const popupCloseEditButton = popupEdit.querySelector('.popup__button-close');
-const popupSaveEditButton = popupEdit.querySelector('.popup__button-save');
 const formElementEdit = popupEdit.querySelector('.popup__form_edit');
 const nameProfile = document.querySelector('.profile__name');
 const statusProfile = document.querySelector('.profile__status');
 const nameInput = document.getElementsByName('name-input')[0];
 const statusInput = document.getElementsByName('status-input')[0];
-
-const initialCards = [{
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
+const cardsContainer = document.querySelector('.gallery__cards');
 
 /*=================PopupEdit=====================*/
 function openPopupEdit() {
@@ -43,7 +17,6 @@ function openPopupEdit() {
 }
 
 function savePopupEdit() {
-    event.preventDefault();
     nameProfile.textContent = nameInput.value;
     statusProfile.textContent = statusInput.value;
     closePopup(popupEdit);
@@ -53,7 +26,8 @@ popupOpenEditButton.addEventListener('click', () => {
     openPopupEdit(popupEdit)
 });
 
-formElementEdit.addEventListener('submit', () => {
+popupEdit.addEventListener('submit', (event) => {
+    event.preventDefault();
     savePopupEdit()
 });
 
@@ -63,26 +37,54 @@ popupCloseEditButton.addEventListener('click', () => {
 
 
 /*=================PopupAdd=====================*/
-const popupAdd = document.querySelector(".popup_add");
+const popupAdd = document.querySelector('.popup_add');
 const popupOpenAddButton = document.querySelector('.profile__add-button');
-const popupCloseAddButton = popupAdd.querySelector(".popup__button-close");
+const popupCloseAddButton = popupAdd.querySelector('.popup__button-close');
+const galleryElement = document.querySelector('.template-gallery').content;
 const mestoInput = document.getElementsByName('mesto-input')[0];
 const urlInput = document.getElementsByName('url-input')[0];
+
 
 function openPopupAdd() {
     mestoInput.value = '';
     urlInput.value = '';
-    openPopup(popupAdd)
-}
+
+    openPopup(popupAdd);
+
+};
+
+function savePopupAdd(name, link) {
+    const card = createCard(name, link);
+    cardsContainer.prepend(card);
+    closePopup(popupAdd);
+};
+
+function createCard(name, link) {
+    const container = galleryElement.cloneNode(true);
+    const image = galleryElement.querySelector('.gallery__image');
+    const title = galleryElement.querySelector('.gallery__card-name');
+
+    title.textContent = name;
+    image.src = link;
+    image.alt = name;
+
+    return container;
+};
+
+
 
 popupCloseAddButton.addEventListener('click', () => {
-    closePopup(popupAdd)
+    closePopup(popupAdd);
 });
 
 popupOpenAddButton.addEventListener('click', () => {
-    openPopupAdd()
+    openPopupAdd();
 });
 
+popupAdd.addEventListener('submit', (event) => {
+    event.preventDefault();
+    savePopupAdd(mestoInput.value, urlInput.value);
+});
 
 /*==============================CommonFunctions===========================*/
 function openPopup(popup) {
