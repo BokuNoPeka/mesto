193f2c5eb2165507215b5fb1e061c44f60e9322a
Artem Koldunov <smorkwriter@yandex.ru>
2020-10-05 17:11:40 +0300

    refactor: создание отдельных файлов для объявления пременных и оптимизация js кода

--- scripts/sctipts.js
+++ scripts/sctipts.js
@@ -1,74 +1,27 @@
-const popup = document.querySelector('.popup');
-const EditForm = document.querySelector('.popup_edit');
-const openEditFormButton = document.querySelector('.profile__edit-button');
-const closeEditFormButton = EditForm.querySelector('.popup__button-close');
-const profileName = document.querySelector('.profile__name');
-const profileStatus = document.querySelector('.profile__status');
-const nameInput = document.getElementsByName('name-input')[0];
-const statusInput = document.getElementsByName('status-input')[0];
-const cardsContainer = document.querySelector('.gallery__cards');
-const AddForm = document.querySelector('.popup_add');
-const openAddFormButton = document.querySelector('.profile__add-button');
-const closeAddFormButton = AddForm.querySelector('.popup__button-close_add');
-const galleryElement = document.querySelector('.template-gallery').content;
-const mestoInput = document.getElementsByName('mesto-input')[0];
-const urlInput = document.getElementsByName('url-input')[0];
-const fullSizeImage = document.querySelector('.popup_full-size');
-const imageName = fullSizeImage.querySelector('.popup__figurecaption');
-const imageSource = fullSizeImage.querySelector('.popup__image');
-const closeFullSizeButton = fullSizeImage.querySelector('.popup__button-close_full-size');
-
-const initialCards = [{
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
-
-
 /*=================PopupEdit=====================*/
 function openEditForm() {
     nameInput.value = profileName.textContent;
     statusInput.value = profileStatus.textContent;
-    openPopup(EditForm);
+    openPopup(editForm);
 }
 
 function saveEditForm() {
     profileName.textContent = nameInput.value;
     profileStatus.textContent = statusInput.value;
-    closePopup(EditForm);
+    closePopup(editForm);
 }
 
 openEditFormButton.addEventListener('click', () => {
-    openEditForm(EditForm);
+    openEditForm(editForm);
 });
 
-EditForm.addEventListener('submit', (event) => {
+editForm.addEventListener('submit', (event) => {
     event.preventDefault();
     saveEditForm();
 });
 
 closeEditFormButton.addEventListener('click', () => {
-    closePopup(EditForm);
+    closePopup(editForm);
 });
 
 
@@ -77,15 +30,15 @@ function openAddForm() {
     mestoInput.value = '';
     urlInput.value = '';
 
-    openPopup(AddForm);
+    openPopup(addForm);
 
-};
+}
 
 function submitAddForm(name, link) {
     const card = createCard(name, link);
     cardsContainer.prepend(card);
-    closePopup(AddForm);
-};
+    closePopup(addForm);
+}
 
 function createCard(name, link) {
     const container = galleryElement.cloneNode(true);
@@ -121,14 +74,14 @@ const likeToggle = function(element) {
 
 
 closeAddFormButton.addEventListener('click', () => {
-    closePopup(AddForm);
+    closePopup(addForm);
 });
 
 openAddFormButton.addEventListener('click', () => {
     openAddForm();
 });
 
-AddForm.addEventListener('submit', (event) => {
+addForm.addEventListener('submit', (event) => {
     event.preventDefault();
     submitAddForm(mestoInput.value, urlInput.value);
 });
@@ -138,7 +91,7 @@ function renderCards() {
         submitAddForm(item.name, item.link);
     })
 
-};
+}
 renderCards();
 /*=================FullSize=====================*/
 function showFullSize(name, link) {
@@ -147,6 +100,7 @@ function showFullSize(name, link) {
     imageSource.src = link;
     imageSource.alt = name;
 }
+
 closeFullSizeButton.addEventListener('click', () => {
     closePopup(fullSizeImage);
 });
