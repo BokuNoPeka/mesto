bf93dbd3bfa3ac249060c2312f3accae71bbb35f
Artem Koldunov <smorkwriter@yandex.ru>
2021-01-20 03:05:20 +0300

    refactor: deleted useless const and move all files to src

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,23 +10,13 @@ const templateSelector = document.querySelector(".template-gallery").content;
 const popups = Array.from(document.querySelectorAll(".popup"));
 const editForm = document.querySelector(".popup_edit");
 const openEditFormButton = document.querySelector(".profile__edit-button");
-const closeEditFormButton = editForm.querySelector(".popup__button-close");
-const profileName = document.querySelector(".profile__name");
-const profileStatus = document.querySelector(".profile__status");
+
 const nameInput = editForm.querySelector(".popup__input_edit_name");
 const statusInput = editForm.querySelector(".popup__input_edit_status");
 const cardsContainer = document.querySelector(".gallery__cards");
-const addForm = document.querySelector(".popup_add");
+
 const openAddFormButton = document.querySelector(".profile__add-button");
-const closeAddFormButton = addForm.querySelector(".popup__button-close_add");
-const mestoInput = addForm.querySelector(".popup__input_add_name");
-const urlInput = addForm.querySelector(".popup__input_add_source");
-const fullSizeImage = document.querySelector(".popup_full-size");
-const imageName = fullSizeImage.querySelector(".popup__figurecaption");
-const imageSource = fullSizeImage.querySelector(".popup__image");
-const closeFullSizeButton = fullSizeImage.querySelector(
-  ".popup__button-close_full-size"
-);
+
 const formEditSelector = ".popup__form_edit";
 const formAddSelector = ".popup__form_add";
 const validationSettings = {
